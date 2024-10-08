3b8c86b133645af6340832b060e2a451f845bc2b
cl7-dp <devang@codelab7.com>
2024-10-08 15:44:41 +0530

    Add Slider library

--- components/BannerImage.tsx
+++ components/BannerImage.tsx
@@ -1,33 +1,29 @@
-import {ScrollView} from "react-native-gesture-handler";
-import {Dimensions, Image} from "react-native";
 import * as React from "react";
-import {useState} from "react";
+import {Dimensions, Image} from "react-native";
+import Swiper from 'react-native-swiper';
+import {ThemedView} from "@/components/ThemedView";
 
-const BannerImage = ({bannerData}) => {
-    const [active, setActive] = useState(0);
+const BannerImage = ({bannerData}) =>{
     const baseImageURL = process.env.EXPO_PUBLIC_IMAGE_URL;
-    const {width} = Dimensions.get('window');
+    const { width } = Dimensions.get('window');
     const height = width * 0.3;
 
-    const onScrollChange = ({nativeEvent}) => {
-        const slide = Math.ceil(
-            nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width,
-        );
-        if (slide !== active) {
-            setActive(slide);
-        }
-    };
-    return (
-        <ScrollView
-            pagingEnabled
-            horizontal
-            onScroll={onScrollChange}
-            showsHorizontalScrollIndicator={false}
-            style={{width, height}}>
+    return(
+        <Swiper
+            showsPagination={false}
+            loop={true}
+            autoplay={true}
+            autoplayTimeout={1}
+        >
             {bannerData.map((item, index) => (
-                <Image key={index} source={{uri: `${baseImageURL}/banner_image/${item.image}`}} style={{width, height, resizeMode: 'cover', marginVertical: 10}} />
+                <ThemedView key={index}>
+                    <Image
+                        source={{ uri: `${baseImageURL}/banner_image/${item.image}` }}
+                        style={{ width: width, resizeMode: 'contain', height:height }}
+                    />
+                </ThemedView>
             ))}
-        </ScrollView>
+        </Swiper>
     )
 }
-export default BannerImage
+export default BannerImage;
